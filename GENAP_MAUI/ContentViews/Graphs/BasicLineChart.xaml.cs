d155fb9bae9e086bfaed2bdf05d1f345e62baa3f
rpfeldman <ramirofeldman0@gmail.com>
2026-05-25 13:32:43 -0300

    Updated BasicLineChart to use TransactionDto as it's bindable property

--- GENAP_MAUI/ContentViews/Graphs/BasicLineChart.xaml.cs
+++ GENAP_MAUI/ContentViews/Graphs/BasicLineChart.xaml.cs
@@ -1,149 +1,202 @@
+using DomainModel;
 using LiveChartsCore;
 using LiveChartsCore.Kernel.Sketches;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using SkiaSharp;
+using System.Globalization;
 
 namespace GENAP_MAUI.ContentViews.Graphs;
 
 public partial class BasicLineChart : ContentView
 {
-    private static readonly SKColor AxisTextColor = SKColor.Parse("#94A3B8");
-    private static readonly SKColor GridLineColor = SKColor.Parse("#263241");
+	private static readonly SKColor AxisTextColor = SKColor.Parse("#94A3B8");
+	private static readonly SKColor GridLineColor = SKColor.Parse("#263241");
 
-    public static readonly BindableProperty ValuesProperty = BindableProperty.Create(
-        nameof(Values),
-        typeof(decimal[]),
-        typeof(BasicLineChart),
-        Array.Empty<decimal>(),
-        propertyChanged: OnDataChanged);
+	private const int MinDaysVisible = 7;
 
-    public decimal[] Values
-    {
-        get => (decimal[])GetValue(ValuesProperty);
-        set => SetValue(ValuesProperty, value);
-    }
+	public static readonly BindableProperty TransactionsProperty = BindableProperty.Create(
+		nameof(Transactions),
+		typeof(List<TransactionDto>),
+		typeof(BasicLineChart),
+		new List<TransactionDto>(),
+		propertyChanged: OnDataChanged);
 
-    public static readonly BindableProperty LineColorProperty = BindableProperty.Create(
-        nameof(LineColor),
-        typeof(Color),
-        typeof(BasicLineChart),
-        Colors.White,
-        propertyChanged: OnColorChanged);
+	public List<TransactionDto> Transactions
+	{
+		get => (List<TransactionDto>)GetValue(TransactionsProperty);
+		set => SetValue(TransactionsProperty, value);
+	}
 
-    public Color LineColor
-    {
-        get => (Color)GetValue(LineColorProperty);
-        set => SetValue(LineColorProperty, value);
-    }
+	public static readonly BindableProperty LineColorProperty = BindableProperty.Create(
+		nameof(LineColor),
+		typeof(Color),
+		typeof(BasicLineChart),
+		Colors.White,
+		propertyChanged: OnDataChanged);
 
-    public ISeries[] LineSeriesCollection { get; private set; }
-    public ICartesianAxis[] XAxes { get; }
-    public ICartesianAxis[] YAxes { get; }
+	public Color LineColor
+	{
+		get => (Color)GetValue(LineColorProperty);
+		set => SetValue(LineColorProperty, value);
+	}
 
-    public BasicLineChart()
-    {
-        LineSeriesCollection =
-        [
-            CreateSeries([0d], ToSkColor(Colors.White))
-        ];
+	public ISeries[] LineSeriesCollection { get; private set; }
+	public ICartesianAxis[] XAxes { get; private set; }
+	public ICartesianAxis[] YAxes { get; }
 
-        XAxes =
-        [
-            new Axis
-            {
-                IsVisible = false
-            }
-        ];
+	private DateOnly[] _dates = [];
 
-        YAxes =
-        [
-            new Axis
-            {
-                Labeler = value => $"{value:N0}$",
-                TextSize = 11,
-                LabelsPaint = new SolidColorPaint(AxisTextColor),
-                SeparatorsPaint = new SolidColorPaint(GridLineColor.WithAlpha(90))
-                {
-                    StrokeThickness = 1
-                }
-            }
-        ];
+	public BasicLineChart()
+	{
+		LineSeriesCollection =
+		[
+			CreateSeries([0d], ToSkColor(Colors.White))
+		];
 
-        InitializeComponent();
-    }
+		XAxes =
+		[
+			new Axis { IsVisible = false }
+		];
 
-    private static void OnDataChanged(BindableObject bindable, object oldValue, object newValue)
-    {
-        var control = (BasicLineChart)bindable;
-        control.UpdateValues();
-    }
+		YAxes =
+		[
+			new Axis
+			{
+				Labeler = value => $"{value:N0}$",
+				TextSize = 11,
+				LabelsPaint = new SolidColorPaint(AxisTextColor),
+				SeparatorsPaint = new SolidColorPaint(GridLineColor.WithAlpha(90))
+				{
+					StrokeThickness = 1
+				}
+			}
+		];
 
-    private static void OnColorChanged(BindableObject bindable, object oldValue, object newValue)
-    {
-        var control = (BasicLineChart)bindable;
-        control.UpdateColor();
-    }
+		InitializeComponent();
+	}
 
-    private void UpdateValues()
-    {
-        var color = ToSkColor(LineColor);
+	private static void OnDataChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		var control = (BasicLineChart)bindable;
+		control.UpdateChart();
+	}
 
-        if (Values is null || Values.Length == 0)
-        {
-            LineSeriesCollection = [CreateSeries([0d], color)];
-            OnPropertyChanged(nameof(LineSeriesCollection));
-            return;
-        }
+	private void UpdateChart()
+	{
+		var color = ToSkColor(LineColor);
 
-        var doubles = new double[Values.Length];
+		if (Transactions is null || Transactions.Count == 0)
+		{
+			_dates = [];
+			LineSeriesCollection = [CreateSeries([0d], color)];
+			XAxes = [new Axis { IsVisible = false }];
+			OnPropertyChanged(nameof(LineSeriesCollection));
+			OnPropertyChanged(nameof(XAxes));
+			return;
+		}
 
-        for (var i = 0; i < Values.Length; i++)
-        {
-            doubles[i] = (double)Values[i];
-        }
+		var (dates, accumulatedValues) = AccumulateByDay(Transactions);
+		_dates = dates;
 
-        LineSeriesCollection = [CreateSeries(doubles, color)];
-        OnPropertyChanged(nameof(LineSeriesCollection));
-    }
+		LineSeriesCollection = [CreateSeries(accumulatedValues, color)];
+		XAxes = [BuildXAxis()];
 
-    private void UpdateColor()
-    {
-        UpdateValues();
-    }
+		OnPropertyChanged(nameof(LineSeriesCollection));
+		OnPropertyChanged(nameof(XAxes));
+	}
 
-    private static LineSeries<double> CreateSeries(double[] values, SKColor color)
-    {
-        return new LineSeries<double>
-        {
-            Values = values,
-            GeometrySize = 0,
-            LineSmoothness = 0.35,
-            Stroke = new SolidColorPaint(color)
-            {
-                StrokeThickness = 3
-            },
-            Fill = new LiveChartsCore.SkiaSharpView.Painting.LinearGradientPaint(
-                new[]
-                {
-                    color.WithAlpha(70),
-                    color.WithAlpha(18),
-                    color.WithAlpha(0)
-                },
-                new SKPoint(0.5f, 0),
-                new SKPoint(0.5f, 1)),
-            GeometryFill = null,
-            GeometryStroke = null,
-            YToolTipLabelFormatter = point => $"{point.Coordinate.PrimaryValue:N2}$"
-        };
-    }
+	private static (DateOnly[] Dates, double[] Values) AccumulateByDay(List<TransactionDto> transactions)
+	{
+		var dailyTotal = transactions
+			.GroupBy(t => t.Date)
+			.OrderBy(g => g.Key)
+			.ToDictionary(
+				g => g.Key,
+				g => g.Sum(t => t.Value));
 
-    private static SKColor ToSkColor(Color color)
-    {
-        return new SKColor(
-            (byte)(color.Red * 255),
-            (byte)(color.Green * 255),
-            (byte)(color.Blue * 255),
-            (byte)(color.Alpha * 255));
-    }
+		var from = dailyTotal.Keys.Min();
+		var to = dailyTotal.Keys.Max();
+
+		if (to.DayNumber - from.DayNumber < MinDaysVisible - 1)
+		{
+			from = to.AddDays(-(MinDaysVisible - 1));
+		}
+
+		var dates = new List<DateOnly>();
+		var values = new List<double>();
+		decimal running = 0;
+
+		for (var day = from; day <= to; day = day.AddDays(1))
+		{
+			if (dailyTotal.TryGetValue(day, out var dayValue))
+			{
+				running += dayValue;
+			}
+			dates.Add(day);
+			values.Add((double)running);
+		}
+
+		return (dates.ToArray(), values.ToArray());
+	}
+
+	private Axis BuildXAxis()
+	{
+		return new Axis
+		{
+			Labeler = value =>
+			{
+				var index = (int)Math.Round(value);
+				if (index < 0 || index >= _dates.Length) return string.Empty;
+				return _dates[index].ToString("dd/MM", CultureInfo.InvariantCulture);
+			},
+			TextSize = 11,
+			LabelsPaint = new SolidColorPaint(AxisTextColor),
+			MinStep = 1,
+			SeparatorsPaint = null
+		};
+	}
+
+	private LineSeries<double> CreateSeries(double[] values, SKColor color)
+	{
+		return new LineSeries<double>
+		{
+			Values = values,
+			GeometrySize = 0,
+			LineSmoothness = 0.35,
+			Stroke = new SolidColorPaint(color)
+			{
+				StrokeThickness = 3
+			},
+			Fill = new LiveChartsCore.SkiaSharpView.Painting.LinearGradientPaint(
+				new[]
+				{
+					color.WithAlpha(70),
+					color.WithAlpha(18),
+					color.WithAlpha(0)
+				},
+				new SKPoint(0.5f, 0),
+				new SKPoint(0.5f, 1)),
+			GeometryFill = null,
+			GeometryStroke = null,
+			YToolTipLabelFormatter = point =>
+			{
+				var index = (int)Math.Round(point.Coordinate.SecondaryValue);
+				var dateLabel = index >= 0 && index < _dates.Length
+					? _dates[index].ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)
+					: string.Empty;
+				var valueLabel = point.Coordinate.PrimaryValue.ToString("N2", CultureInfo.InvariantCulture);
+				return $"{dateLabel}\n{valueLabel}$";
+			}
+		};
+	}
+
+	private static SKColor ToSkColor(Color color)
+	{
+		return new SKColor(
+			(byte)(color.Red * 255),
+			(byte)(color.Green * 255),
+			(byte)(color.Blue * 255),
+			(byte)(color.Alpha * 255));
+	}
 }
