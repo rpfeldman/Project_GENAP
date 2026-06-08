bf18995dbcaff12ef4390ec0afb6d17165315f9a
rpfeldman <ramirofeldman0@gmail.com>
2026-06-08 13:26:37 -0300

    Updated Colors.xaml and Styles.xaml

--- GENAP_MAUI/obj/Debug/net10.0-android/GENAP_MAUI.AssemblyInfo.cs
+++ GENAP_MAUI/obj/Debug/net10.0-android/GENAP_MAUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GENAP_MAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+da6fa59393bba10d8726d3324a0a579f855bd6a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0ef45e7051ba2ade6cca233df49d3b06badf47c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GENAP_MAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GENAP_MAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
