d09654a42cbd0ab72566dbcc44806c41013565ab
rpfeldman <ramirofeldman0@gmail.com>
2026-07-09 13:59:08 -0300

    add the logic for the ClearStorageConfirmationPage and it's xaml structure

--- GENAP_MAUI/ViewModels/ClearStorageConfirmationPageViewModel.cs
+++ GENAP_MAUI/ViewModels/ClearStorageConfirmationPageViewModel.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using DataServices;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GENAP_MAUI.ViewModels
 {
-    public sealed partial class ClearStorageConfirmationPageViewModel : BaseViewModel
+    public sealed partial class ClearStorageConfirmationPageViewModel(DataManagementService dataManagementService) : BaseViewModel
     {
+        private DataManagementService _dataManagementService = dataManagementService;
 
+        [RelayCommand]
+        public async Task ClearStorage()
+        {
+            var alert = await Shell.Current.DisplayAlertAsync("Confirmar", "¿desea confirmar el reinicio de los datos?", "Si, reinciar", "No cancelar");
+
+            if (!alert){ return; }
+
+            var clearStorageOperation = await _dataManagementService.RestartDataAsync();
+
+            if (!clearStorageOperation.Success)
+            {
+                await Shell.Current.DisplayAlertAsync("Error", clearStorageOperation.ErrorMessage, "Aceptar");
+                return;
+            }
+
+            await Shell.Current.DisplayAlertAsync("Exito", "Se han reiniciado los datos correctamente\n\nVolviendo al menu...", "aceptar");
+            await DirectNavigate(Routes.Dashboard);
+        }
     }
 }
